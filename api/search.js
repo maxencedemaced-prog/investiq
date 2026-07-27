5dde29d9ceb2a42c94035b2c4a6d385ef7231c16
maxencedemaced-prog <maxencedemaced@gmail.com>
2026-07-27 12:32:04 +0200

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,3 +1,9 @@
+const ALLOWED_ORIGINS = [
+  'https://investiq-kappa.vercel.app',
+  'http://localhost:3000',
+  'http://127.0.0.1:5500',
+];
+
 export default async function handler(req, res) {
   const origin = req.headers.origin || '';
   if (ALLOWED_ORIGINS.includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin);
