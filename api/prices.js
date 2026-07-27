4a2475b0405eb9fa4ccfc04b8e8a3bde724669e1
maxencedemaced-prog <maxencedemaced@gmail.com>
2026-07-27 11:39:48 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin || '';
+  if (ALLOWED_ORIGINS.includes(origin)) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
